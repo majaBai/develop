cf25dc26d7aa1061e099f774dc095f6a7216b359
majaBai <452712285@qq.com>
2020-11-22 14:40:07 +0800

    new content in git_gitHub

--- git_gitHub.js
+++ git_gitHub.js
@@ -27,6 +27,7 @@
         首先， 在 git 2.XX 版本前，git add. 不会添加删除的文件到缓存区；但是更高版本的 git 已经不存在这个区别；
         其次，git add . 這個指令會把目前當下這個目錄，以及它的子目錄、子子目錄、子子子目錄…裡的異動全部加到暫存區，但在這個目錄的以外的就不歸它管了。
               而 git add --all 指令就沒這個問題，這個指令不管在專案的哪一層目錄執行，效果都是一樣的，在這個專案裡所有的異動都會被加至暫存區。
+
     2） git commit -m 'some comments' 会把缓存区的内容提交到仓库（仓库在本地还是远程呢？？本地）
         通过 add - commit 指令 git 在背后做了什么？
 
@@ -37,6 +38,7 @@
 
     3) git log
         git log --oneline --graph 更精简的查看日志
+        git log -p xxx.txt 查看特定某个文件的详细记录
         git log --oneline --author="Sherly" 指定作者
         git log --oneline --grep="xxx"  可以從 Commit 訊息裡面搜尋符合 xxx 的內容
         git log -S "xxx" 可以搜尋在所有 Commit 的檔案中，哪些符合特定條件的
@@ -47,25 +49,46 @@
        git rm xxx --cached 并不是真的删除文件，而是让文件 xxx 脱离 git 的管控
        rm xxx.txt 删除文件，但要使用 git add -> git commit 来记录这次追踪
 
-   5)  恢复删除的文件
+   5)  恢复删除的文件 git checkout
+        git checkout branchName 的時候，Git 會切換到指定的分支，但如果後面接的是檔名或路徑，Git 則不會切換分支，而是把檔案從 .git 目錄裡拉一份到目前的工作目錄。
+        更精準的來說，這個指令會把暫存區（Staging Area）裡的內容或檔案，拿來覆蓋工作目錄（Working Directory）的內容或檔案。
+        所以當在上面執行 git checkout welcome.html 或 git checkout . 的時候，它會把 welcome.html 這個檔案，或是當下目錄所有檔案回復到上一次 Commit 的狀態。
+
+        如果 git checkout HEAD~2 welcome.html 這樣就會拿距離現在兩個版本以前的那個 welcome.html 檔案來覆蓋現在的工作目錄裡的 welcome.html 檔案，但要注意的是，這同時也會更新暫存區的狀態喔。
+            git checkout HEAD~2 . 拿距離現在兩個版本以前的檔案來覆蓋現在工作目錄的檔案，同時也更新暫存區裡的狀態
+
+
       使用 rm 删除文件，但还未 add，可使用git checkout xxx.txt 恢复
       使用 git rm 删除的文件， 首先 git restore --staged xxx.txt -> git checkout xxx.txt 恢复
 
     6) git mv a.txt b.txt 将 a.txt 重命名为 b.txt
 
     7) 要修改 Commit 紀錄有好幾種方法：
-        把 .git 目錄整個刪除（會把該專案所有的 Git 紀錄全部清掉，除非必要，不要輕易使用）。
-        使用 git rebase 來修改歷史。
-        先把 Commit 用 git reset 拆掉，整理後再重新 Commit。
-        使用 --amend 參數來修改最後一次的 Commit。
+        a. 把 .git 目錄整個刪除（會把該專案所有的 Git 紀錄全部清掉，除非必要，不要輕易使用）。
+        b. 使用 git rebase 來修改歷史。
+        c. 先把 Commit 用 git reset 拆掉，整理後再重新 Commit。
+            git reset 有三个参数可选，他们會決定「Commit 拆出來的那些檔案何去何從」
+                模式	              mixed 模式（默认）	soft 模式	    hard 模式
+            Commit 拆出來的檔案	       丟回工作目錄	        丟回暫存區	     直接丟掉
+
+
+        d. 使用 --amend 參數來修改最後一次的 Commit。
 
        修改 commit 记录时，git会将其看做时一次的新的 commit，会生成新的 SHA-1 值；儘量不要在已經 Push 之後再修改 commit
+
     8) 追加文件到最近一次commit中
        方法1： git reset
        方法2：在commit 新文件时使用 git commit --amend --no-edit
+
     9) 新增文件夹
         請記得一件很重要的觀念，就是 Git 在計算、產生物件的時候，是根據「檔案的內容」去做計算的，所以光是新增一個目錄，Git 是沒辦法處理它的。
         空的目錄無法被提交！
         解决方案：慣例上可以放一個名為 “.keep” 或 “.gitkeep” 的空檔案，讓 Git 能「感應」到這個目錄的存在（windows上貌似不行，可在新建的目录下添加一个readme.txt, 确保文件中有内容）
+
+    10) .gitignor 忽略不需要进行版本管理的文件
+
+    11） git blame xxx.txt 可以查看谁动过xxx.txt
+
+    12）
 
 */
